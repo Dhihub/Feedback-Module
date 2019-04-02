71de87e1b12f42bb084a65a18412230599dfdf20
rmit-s3635887-pavan-w <37763559+s3635887@users.noreply.github.com>
2019-04-02 18:55:26 +1100

    minor changes

--- Dhihub/slack.py
+++ Dhihub/slack.py
@@ -1,6 +1,6 @@
 import requests
 import json
-web_hook_url = 'https://hooks.slack.com/services/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'
+web_hook_url = 'https://hooks.slack.com/services/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'
 msg = {'text':'from python and slack'}
 
 requests.post(web_hook_url,data=json.dumps(msg))
--- Dhihub/slack2.py
+++ Dhihub/slack2.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-slack_token = 'xoxb-575060474148-585358061488-sKHNxUnHSwFO2qehLyEgWIZj'
+slack_token = 'xoxb-575060474148-585358061488-uSAy4gzktu8jztx3154DzKdr'
 sc = SlackClient(slack_token)
 
 
@@ -16,12 +16,15 @@ def index():
 @app.route("/send", methods=["POST"])
 def send_message():
     message  = request.form['Message']
-    print(message)
     sc = SlackClient(slack_token)
-    sc.api_call(
+    print(sc.api_call
+    (
         "chat.postMessage",
-        channel = "@teamchat",
-        text = message
+        channel = "DHAQX462K",
+        text = message,
+        attachments = [{"pretext": "Would you like to play a game?"}],
+        as_user = True	
+    )
     )
 
     return render_template("trail2.html")
@@ -30,3 +33,42 @@ if __name__ == '__main__':
     app.debug = True
     app.run()
 
+{
+    "text": "",
+    "attachments": [
+        {
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "game",
+                    "text": "Chess",
+                    "type": "button",
+                    "value": "chess"
+                },
+                {
+                    "name": "game",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                },
+                {
+                    "name": "game",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ]
+        }
+    ]
+}
